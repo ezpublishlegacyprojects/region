ea3e8ea432970eb29f4941e44697346e2d97603d
Björn Dieding <bjoern@xrow.de>
2009-04-15 15:19:01 +0000

    loader
    git-svn-id: http://svn.projects.ez.no/region@26 11d0b303-553a-4116-9672-2c3f617efcea

--- trunk/classes/ezxregion.php
+++ trunk/classes/ezxregion.php
@@ -1,6 +1,132 @@
 <?
+
 class ezxRegion
 {
+
+    /**
+     * Returns Region information for the current user/ip...
+     *
+     * @return array Returns an array with keys
+     */
+    static function load( $ignore_list = array(), $SessionName = 'eZSESSID' )
+    {
+
+        if ( self::isBot() )
+        {
+            return;
+        }
+        
+        if ( array_key_exists( $SessionName, $_COOKIE ) )
+        {
+            return;
+        }
+        $urlCfg = new ezcUrlConfiguration( );
+        #$urlCfg->basedir = 'mydir';
+        $urlCfg->script = 'index.php';
+        $url = new ezcUrl( ezcUrlTools::getCurrentUrl(), $urlCfg );
+        $params = $url->getParams();
+        if ( file_exists( 'settings/siteaccess/' . $params[0] ) )
+        {
+            $siteaccess = $params[0];
+        }
+        else
+        {
+            $siteaccess = false;
+        }
+        
+        if ( ( $params[0] == 'region' and $params[1] == 'index' ) or ( $siteaccess and $params[1] == 'region' and $params[2] == 'index' ) )
+        {
+            return;
+        }
+        if ( in_array( $params[0], $ignore_list ) )
+        {
+            return;
+        }
+        if ( $siteaccess )
+        {
+            $paramnew = array( 
+                $siteaccess , 
+                'region' , 
+                'index' , 
+                $siteaccess 
+            );
+        }
+        else
+        {
+            $paramnew = array( 
+                'region' , 
+                'index' 
+            );
+        }
+        $query = $url->getQuery();
+        $params = $url->path;
+        if ( $siteaccess )
+        {
+            array_shift( $params );
+        }
+
+        if( count( $params ) )
+        {
+            $query['URL'] = join( '/', $params );
+        }
+
+        $url->setQuery( $query );
+        $url->params = $paramnew;
+ 
+        eZHTTPTool::redirect( $url->buildUrl() );
+    
+    }
+
+    static function isBot()
+    {
+        $bot_list = array( 
+            "Teoma" , 
+            "alexa" , 
+            "froogle" , 
+            "Gigabot" , 
+            "inktomi" , 
+            "looksmart" , 
+            "URL_Spider_SQL" , 
+            "Firefly" , 
+            "NationalDirectory" , 
+            "Ask Jeeves" , 
+            "TECNOSEEK" , 
+            "InfoSeek" , 
+            "WebFindBot" , 
+            "girafabot" , 
+            "crawler" , 
+            "www.galaxy.com" , 
+            "Googlebot" , 
+            "Scooter" , 
+            "Slurp" , 
+            "msnbot" , 
+            "appie" , 
+            "FAST" , 
+            'Slurp' , 
+            'CazoodleBot' , 
+            'msnbot' , 
+            'InfoPath' , 
+            'Baiduspider' , 
+            "WebBug" , 
+            "Spade" , 
+            "ZyBorg" , 
+            "rabaz" , 
+            "Baiduspider" , 
+            "Feedfetcher-Google" , 
+            "TechnoratiSnoop" , 
+            "Rankivabot" , 
+            "Mediapartners-Google" , 
+            "Sogou web spider" , 
+            "WebAlta Crawler" 
+        );
+        
+        if ( preg_match( "/" . join( '|', $bot_list ) . "/", $_SERVER['HTTP_USER_AGENT'] ) )
+        {
+            return true;
+        }
+        return false;
+    }
+
     /**
      * Returns Region information for the current user/ip...
      *
@@ -8,25 +134,23 @@ class ezxRegion
      */
     static function getRegionData( $address = null )
     {
-    	eZDebug::writeDebug( 'Starting...', 'ezxRegion::getRegionData()' );
+        eZDebug::writeDebug( 'Starting...', 'ezxRegion::getRegionData()' );
         $regionini = eZINI::instance( 'region.ini' );
         $regions = $regionini->groups();
         unset( $regions['Settings'] );
         $ccode = ezxISO3166::preferredCountry( $address );
         $lcode = ezxISO936::preferredLanguages();
         $regions_keys = array_keys( $regions );
-        $preferred_regions = array( 
-        );
+        $preferred_regions = array();
         
         foreach ( $regions as $key => $region )
         {
             if ( $ccode and strpos( $key, '_' . $ccode ) !== false )
                 $preferred_regions[$key] = $region;
         }
-
+        
         $langs = array_keys( $lcode );
-        $preferred_languages = array( 
-        );
+        $preferred_languages = array();
         foreach ( $regions as $key => $region )
         {
             foreach ( $langs as $lang )
@@ -63,8 +187,8 @@ class ezxRegion
             return false;
         }
         return array( 
-            'preferred_region' => $preferred_region,
-            'preferred_languages' => $preferred_languages, 
+            'preferred_region' => $preferred_region , 
+            'preferred_languages' => $preferred_languages , 
             'preferred_regions' => $preferred_regions 
         );
     }
